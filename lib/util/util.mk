b952d80d7932e7c8b0d7e9c6f1940be70e1f482a
Deadcafe Beef <deadcafe.beef@gmail.com>
2023-06-02 14:34:09 +0900

    fixed util.mk

--- lib/util/util.mk
+++ lib/util/util.mk
@@ -1,1 +1,2 @@
-UTIL_OBJS := params.o logging.o util.o stats.o xpcapng.o
+#UTIL_OBJS := params.o logging.o util.o stats.o xpcapng.o
+UTIL_OBJS := $(addprefix $(OBJ_DIR)/, params.o logging.o util.o stats.o xpcapng.o)
